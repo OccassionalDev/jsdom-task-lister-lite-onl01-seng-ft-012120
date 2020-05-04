26167f599e6e77893d3d1ca99a64acc1c782d178
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-04 18:20:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTask = document.getElementById("create-task-form");
   const newDescription = document.getElementById("new-task-description");
   
+  // Form event listener 
+  
+  
   
   
   
